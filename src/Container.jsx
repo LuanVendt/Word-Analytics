67dd7b5c10210c9a96e5c8bc92b9379520415d54
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-20 11:55:00 -0300

    feat: implement controlled TextArea and enhance Stats component for word and character counting

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
 
 export default function Container() {
+  const [text, setText] = useState("");
+
+  const numberOfWords = text.split(" ").filter((word) => word !== "").length;
+
   return (
     <main className="container">
-      <TextArea />
-      <Stats />
+      <TextArea text={text} setText={setText} />
+      <Stats numberOfWords={numberOfWords} numberOfCharacters={text.length} />
     </main>
   );
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,24 @@
-export default function Stats() {
-  return <section className="stats">Stats</section>;
+export default function Stats({ numberOfWords = 0, numberOfCharacters = 0 }) {
+  return (
+    <section className="stats">
+      <Stat number={numberOfWords} label={"Words"} />
+      <Stat number={numberOfCharacters} label={"Characters"} />
+      <Stat number={280 - numberOfCharacters} label={"Instagram"} />
+      <Stat number={2200 - numberOfCharacters} label={"Facebook"} />
+    </section>
+  );
+}
+
+function Stat({ number, label }) {
+  return (
+    <section className="stat">
+      <span
+        className="stat__number"
+        style={{ color: number < 0 ? "red" : "inherit" }}
+      >
+        {number}
+      </span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
 }
