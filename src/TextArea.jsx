4ca56e2450b36d4da957a08e1ff6dc3786dab1d4
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-20 10:58:54 -0300

    feat: add warning for disallowed script tags in TextArea and create Warning component

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,17 +1,33 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function TextArea() {
   const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+  const [warningText, setWarningText] = useState("");
+
+  const handleChangeEvent = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes("<script>")) {
+      setShowWarning(true);
+      setWarningText("Scripts are not allowed!");
+
+      newText = newText.replace("<script>", "");
+    }
+
+    setText(newText);
+  };
 
   return (
-    <textarea
-      value={text}
-      onChange={(e) => {
-        setText(e.target.value);
-      }}
-      className="textarea"
-      placeholder="Enter your text..."
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChangeEvent}
+        placeholder="Enter your text..."
+        spellCheck="false"
+      />
+      {showWarning ? <Warning text={warningText} /> : null}
+    </div>
   );
 }
